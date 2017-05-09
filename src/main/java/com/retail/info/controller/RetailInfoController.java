c8210edd59ea2d1bf2023cfd0e2b7eba1176b66a
kaustavb7 <kaustavb7@gmail.com>
2017-05-09 23:49:55 +0530

    Changes to mysql

--- src/main/java/com/retail/info/controller/RetailInfoController.java
+++ src/main/java/com/retail/info/controller/RetailInfoController.java
@@ -3,6 +3,7 @@ package com.retail.info.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +14,40 @@ import com.retail.info.repository.RetailRepository;
 public class RetailInfoController {
 	
 	@Autowired
-	RetailRepository rp;
+	RetailRepository retailRepository;
 	
-	@RequestMapping("/Hello")
+	@RequestMapping("/findAll")
 	public List<RetailInfo> productDisplay()
 	{
-		return rp.findAll();
+		return retailRepository.findAll();
 	}
-
+	
+	@RequestMapping("/findOne/{id}")
+	public RetailInfo findById(@PathVariable("id") Integer retailId) {
+        return retailRepository.findOne(retailId);
+    }
+ 
+   /* public RetailInfo findByName(String productName) {
+        return retailRepository.findByName(productName);
+    }*/
+	
+	@RequestMapping("/save")
+	public void saveUser(RetailInfo retailInfo) {
+		retailRepository.save(retailInfo);
+    }
+	
+	@RequestMapping("/updateUser")
+	public void updateUser(RetailInfo retailInfo){
+        	saveUser(retailInfo);
+    }
+ 
+	@RequestMapping("/delete")
+    public void deleteUserById(int retailId){
+    	retailRepository.delete(retailId);
+    }
+ 
+	@RequestMapping("/deleteAll")
+    public void deleteAllUsers(){
+    	retailRepository.deleteAll();
+    }
 }
--- src/main/java/com/retail/info/domain/RetailInfo.java
+++ src/main/java/com/retail/info/domain/RetailInfo.java
@@ -15,39 +15,86 @@ public class RetailInfo {
     @GeneratedValue(strategy=GenerationType.AUTO)
 	private int retailId;
 	private String productName;
-	private int productPrice;
-	
-	public RetailInfo(int retailId, String productName, int productPrice) {
-		super();
-		this.retailId = retailId;
-		this.productName = productName;
-		this.productPrice = productPrice;
-	}
-	
+	private String productPrice;
+	private String productRating;
+	private String productStatus;
+	private String productStartTime;
+	private String productEndTime;
 	
 	public RetailInfo() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
+	public RetailInfo(int retailId, String productName, String productPrice, String productRating, String productStatus,
+			String productStartTime, String productEndTime) {
+		super();
+		this.retailId = retailId;
+		this.productName = productName;
+		this.productPrice = productPrice;
+		this.productRating = productRating;
+		this.productStatus = productStatus;
+		this.productStartTime = productStartTime;
+		this.productEndTime = productEndTime;
+	}
 
 	public int getRetailId() {
 		return retailId;
 	}
+
 	public void setRetailId(int retailId) {
 		this.retailId = retailId;
 	}
+
 	public String getProductName() {
 		return productName;
 	}
+
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
-	public int getProductPrice() {
+
+	public String getProductPrice() {
 		return productPrice;
 	}
-	public void setProductPrice(int productPrice) {
-		this.productPrice = productPrice;
-	} 
 
+	public void setProductPrice(String productPrice) {
+		this.productPrice = productPrice;
+	}
+
+	public String getProductRating() {
+		return productRating;
+	}
+
+	public void setProductRating(String productRating) {
+		this.productRating = productRating;
+	}
+
+	public String getProductStatus() {
+		return productStatus;
+	}
+
+	public void setProductStatus(String productStatus) {
+		this.productStatus = productStatus;
+	}
+
+	public String getProductStartTime() {
+		return productStartTime;
+	}
+
+	public void setProductStartTime(String productStartTime) {
+		this.productStartTime = productStartTime;
+	}
+
+	public String getProductEndTime() {
+		return productEndTime;
+	}
+
+	public void setProductEndTime(String productEndTime) {
+		this.productEndTime = productEndTime;
+	}
+	
+	
+	
+	
 }
